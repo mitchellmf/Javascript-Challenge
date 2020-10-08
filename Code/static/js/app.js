aad74f7e6f7c70358a5dbf39afe49dcf1c8dc2d0
mitchellmf <38438850+mitchellmf@users.noreply.github.com>
2020-10-07 18:26:18 -0700

    Add files via upload

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,35 +1,40 @@
 // from data.js ;
 
-// Arrays to hold movie scores
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-
+function buildtable(data) {
+  data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+  
+      // Append a cell to the row for each value in the UFO sighting object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
 
 // //////////////////////////////////////////
 
-
-// Get a reference to the table body
-var tbody = d3.select("tbody");
 
 function handleClick () {
 // Step 5: Use d3 to update each cell's text with UFO sighting values (weekday, date, high, low)
 console.log("clicked")
 var selected_datetime = d3.select("#datetime").property("value")
-filtered = data.filter(row => row.datetime == selected_datetime)
+if (selected_datetime) {
+  filtered = data.filter(row => row.datetime == selected_datetime)
+}
+else {
+filtered = data
+}
 
 tbody.html("")
-
-filtered.forEach(function(sighting) {
-  console.log(sighting);
-  var row = tbody.append("tr");
-  Object.entries(sighting).forEach(function([key, value]) {
-    console.log(key, value);
-
-    // Append a cell to the row for each value in the UFO sighting object
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
-
+buildtable(filtered)
 }
+
+buildtable(data)
 
 d3.selectAll("#filter-btn").on("click", handleClick);
